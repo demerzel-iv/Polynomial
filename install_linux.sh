fdc16099e75f1ed72265b0139b662cc330038498
demerzel-iv <demerzel_iv@qq.com>
2020-07-14 11:27:29 +0800

    修复安装脚本的bug

--- install_linux.sh
+++ install_linux.sh
@@ -1,4 +1,4 @@
-heads="Imaginary.h Int.h Double.h Image.h element.h poly.h polymath.h polyvar.h"
+heads="Imaginary.h Int.h Double.h Image.h element.h poly.h polymath.h polyvar.h polynomial.h"
 hpath=`pwd`
 
 make lib
@@ -15,27 +15,25 @@ cp $heads /usr/include/poly
 
 cd /etc
 
-if [[ $CPLUS_INCLUDE_PATH == *"/usr/include/poly"* ]] || [[ $LD_LIBRARY_PATH == *"/usr/lib/poly"* ]] || [[ $LIBRARY_PATH == *"/usr/lib/poly"* ]] ; 
-then 
-    flag=1
-else 
-    flag=0
-fi
-if [ $flag == 1 ] ; then 
+if [[ `cat profile` == *"/usr/include/poly"* ]] ; then flag0=1 ; else flag0=0 ; fi
+if [[ `cat profile` == *"/usr/lib/poly"* ]]     ; then flag1=1 ; else flag1=0 ; fi
+if [[ `cat profile` == *"/usr/lib/poly"* ]]     ; then flag2=1 ; else flag2=0 ; fi
+ 
+if [ $flag0 == 0 ] || [ $flag1 == 0 ] || [ $flag2 == 0 ]; then 
     echo "# for poly" >> profile
 fi 
-if [[ $CPLUS_INCLUDE_PATH == *"/usr/include/poly"* ]] ;then 
+if [ $flag0 == 0 ] ;then 
     echo "CPLUS_INCLUDE_PATH=\$CPLUS_INCLUDE_PATH:/usr/include/poly" >> profile
     echo "export CPLUS_INCLUDE_PATH" >> profile
 fi
-if [[ $LD_LIBRARY_PATH == *"/usr/lib/poly"* ]] ;then 
+if [ $flag1 == 0 ] ;then 
     echo "LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/lib/poly" >> profile
     echo "export LD_LIBRARY_PATH" >> profile
 fi
-if [[ $LIBRARY_PATH == *"/usr/lib/poly"* ]] ;then 
+if [ $flag2 == 0 ] ;then 
     echo "LIBRARY_PATH=\$LIBRARY_PATH:/usr/lib/poly" >> profile
     echo "export LIBRARY_PATH" >> profile
 fi
-if [ $flag == 1 ] ; then 
+if [ $flag0 == 0 ] || [ $flag1 == 0 ] || [ $flag2 == 0 ]; then 
     echo "" >> profile
 fi 
