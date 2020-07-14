2ca582d3f9b5dde4ed4f6827a64b12683665c47f
demerzel-iv <demerzel_iv@qq.com>
2020-07-14 09:58:30 +0800

    更好的install_linux.sh

--- install_linux.sh
+++ install_linux.sh
@@ -1,8 +1,8 @@
-codes="Imaginary.cpp Int.cpp Double.cpp Image.cpp element.cpp poly.cpp polymath.cpp polyvar.cpp"
 heads="Imaginary.h Int.h Double.h Image.h element.h poly.h polymath.h polyvar.h"
 hpath=`pwd`
 
-g++ -shared -fPIC $codes -o libpoly.so
+make lib
+make staticlib
 
 cd /usr/lib
 if ! [ -d poly ] ; then mkdir poly ;fi
@@ -10,15 +10,32 @@ cd /usr/include
 if ! [ -d poly ] ; then mkdir poly ;fi
 
 cd $hpath
-cp ./libpoly.so  /usr/lib/poly
+cp ./libpoly.so ./libpoly.a /usr/lib/poly
 cp $heads /usr/include/poly
 
 cd /etc
-echo "# for poly"
-echo "CPLUS_INCLUDE_PATH=\$CPLUS_INCLUDE_PATH:/usr/include/poly" >> profile
-echo "export CPLUS_INCLUDE_PATH" >> profile
-echo "LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/lib/poly" >> profile
-echo "export LD_LIBRARY_PATH" >> profile
-echo "LIBRARY_PATH=\$LIBRARY_PATH:/usr/lib/poly" >> profile
-echo "export LIBRARY_PATH" >> profile
-echo "" >> profile
+
+if [[ $CPLUS_INCLUDE_PATH == *"/usr/include/poly"* ]] && [[ $LD_LIBRARY_PATH == *"/usr/lib/poly"* ]] && [[ $LIBRARY_PATH == *"/usr/lib/poly"* ]] ; 
+then 
+    flag=1
+else 
+    flag=0
+fi
+if [ $flag == 1 ] ; then 
+    echo "# for poly" >> profile
+fi 
+if [[ $CPLUS_INCLUDE_PATH == *"/usr/include/poly"* ]] ;then 
+    echo "CPLUS_INCLUDE_PATH=\$CPLUS_INCLUDE_PATH:/usr/include/poly" >> profile
+    echo "export CPLUS_INCLUDE_PATH" >> profile
+fi
+if [[ $LD_LIBRARY_PATH == *"/usr/lib/poly"* ]] ;then 
+    echo "LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/lib/poly" >> profile
+    echo "export LD_LIBRARY_PATH" >> profile
+fi
+if [[ $LIBRARY_PATH == *"/usr/lib/poly"* ]] ;then 
+    echo "LIBRARY_PATH=\$LIBRARY_PATH:/usr/lib/poly" >> profile
+    echo "export LIBRARY_PATH" >> profile
+fi
+if [ $flag == 1 ] ; then 
+    echo "" >> profile
+fi 
